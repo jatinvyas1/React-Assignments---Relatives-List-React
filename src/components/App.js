130a446870993eafa90580065c42c507a99e0346
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-09 21:06:00 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.relativesList = ["Lal", "Bal", "Pal"];
+  }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {this.relativesList.map((element, index) => {
+            return <li key={"relativeListItem" + (index + 1)}>{element}</li>;
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
